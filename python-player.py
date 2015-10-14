6e506a12bcd101aa30851ac8049448cd88103b6c
Renchon <Renchon@Renchons-MacBook-Pro.local>
2015-10-14 17:03:21 -0400

    Fixed code structure

--- python-player.py
+++ python-player.py
@@ -16,67 +16,30 @@ try:
 except ImportError:
     flags = None
 
+results = []
+gauth = ""
 SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
 CLIENT_SECRET_FILE = 'client_secrets.json'
 APPLICATION_NAME = 'Drive API Python Quickstart'
 
-
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'drive-python-quickstart.json')
-
-    store = oauth2client.file.Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatability with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
-
-def main():
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('drive', 'v2', http=http)
-
-
-    gauth = GoogleAuth()
-    gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication
-
-    results = service.files().list(maxResults=10).execute()
-    items = results.get('items', [])
+def display_music_list(results):
     music = []
-    if not items:
+    if not results:
         print('No files found.')
     else:
         print('Music:')
         i = 1
-        for item in items:
+        for item in results:
             if '.mp3' in item['title']:
                 print('{0}: {1} ({2})'.format(i, item['title'], item['id']))
                 music.append(item)
                 i = i + 1
         pick_file = input('Choose file to play: \n')
         pick_file = pick_file-1
-        play_music(gauth, music[pick_file])
-                
+        play_music(music[pick_file])
 
-def play_music(gauth, item):
+
+def play_music(item):
     drive = GoogleDrive(gauth) # Create GoogleDrive instance with authenticated GoogleAuth instance
     path = os.getcwd()
     file6 = drive.CreateFile({'id': item['id']}) # Initialize GoogleDriveFile instance with file id
@@ -89,8 +52,22 @@ def play_music(gauth, item):
     
     player = pyglet.media.Player()
     music = pyglet.media.load(music_path)
-    music.play()
-    pyglet.app.run()
+    
+    try:
+        music.play()
+        pyglet.app.run()
+    except:
+        pyglet.app.exit
+        display_music_list(results)
+
+def main():
+    global results
+    global gauth
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication
+    drive = GoogleDrive(gauth)
+    results = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+    display_music_list(results)
 
 if __name__ == '__main__':
     main()
